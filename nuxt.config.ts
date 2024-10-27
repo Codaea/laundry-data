1c2f1677428474e23ef568e8a5302e521a58ee8b
Codaea <dakotajayroth@gmail.com>
2024-10-27 03:01:44 +0000

    added nuxt icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,23 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/icon'],
+
+  nitro: {
+    experimental: {
+      database: true,
+    },
+
+    database: {
+      default: {
+        connector: 'postgresql',
+        options: { name: 'db'}
+      },
+      users: {
+        connector: 'postgresql', // https://nitro.unjs.io/guide/database
+      }
+    }
+  }
+
+
 })
