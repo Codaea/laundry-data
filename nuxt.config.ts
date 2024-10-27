36808da08ec40a7dd183b2e865abe78a42220627
Codaea <dakotajayroth@gmail.com>
2024-10-27 03:18:12 +0000

    PWA scaffolding

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,8 +20,36 @@ export default defineNuxtConfig({
     }
   },
 
-  pwa: {
-    
+  pwa: {// gives access to a $pwa object in your components https://vite-pwa-org.netlify.app/frameworks/nuxt.html
+    manifest: {
+      short_name: 'Laundry',
+      name: 'OIT Laundry',
+      icons: [
+        {
+          src: 'https://picsum.photos/96/96',
+          sizes: '96x96',
+          type: 'image/png',
+        },
+        {
+          src: 'https://picsum.photos/192/192',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'https://picsum.photos/512/512',
+          sizes: '512x512',
+          type: 'image/png',
+        }
+      ],
+      start_url: '/',
+      display: 'standalone',
+      theme_color: '#000000',
+      background_color: '#ffffff',
+    },
+
+    devOptions: {
+      enabled: true,
+    }
   }
 
 
